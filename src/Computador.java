3dc6c058d28518e49c6eba7a7bd56de689e6850b
LuiS Felipe Barbarini <112336724+LFelipe19@users.noreply.github.com>
2024-03-08 21:36:52 -0300

    Update Computador.java

--- src/Computador.java
+++ src/Computador.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class Computador {
+    //1. Atributos
+    public String marca;
+    public String cor;
+    public String modelo;
+    public long numeroSerie;
+    public double preco;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    //2; Metodos
+
+
+    @Override
+    public String toString() {
+        return  "Dados do computador: \n " +
+                "Marca: " + marca + " \n " +
+                "Modelo: " + modelo + " \n " +
+                "Cor: " + cor + " \n " +
+                "Preço: " + preco + " \n " +
+                "Número de Série: " + numeroSerie + " \n " ;
+
     }
+
+
+    public  void  calcularValor (){
+        if ( marca.equalsIgnoreCase ( "HP" )){
+            preco *= 1.30;
+            return;
+        }
+        if ( marca.equalsIgnoreCase ( "IBM" )){
+            preco *= 1.50;
+        }
+
+    }
+
+    public  boolean  alterarValor ( double  valor ){
+        if ( valor > 0 ){
+            preco = valor ;
+            return  true ;
+        }
+        return  false ;
+    }
+
+
 }
